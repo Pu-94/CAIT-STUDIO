cb4def5c8633f4ddbfceb7813ecaee20d3bff1d1
Pu-94 <13790683766@139.com>
2025-11-01 21:42:33 +0800

    Update appwrite-init.js

--- Main/appwrite-init.js
+++ Main/appwrite-init.js
@@ -12,7 +12,7 @@ window.addEventListener('load', async () => {
     client
         .setEndpoint('https://sgp.cloud.appwrite.io/v1') // 新加坡端点
         .setProject('6905e79900302369f9c6') // 你的项目ID（正确）
-        .setKey('standard_43ae2735c77e33e0f0b04c60a35e84511839b5ec22dec5f2a119d98ca6402e7163f0e9140081d77c78cb31bf79af97f54f12eef91e290d70f7e98181f77455b8eb415b2f9747b129865bd5a8fa38a1b62d54788f68e21c1e496006ef885fb035a1d15836a80956b9813f58358f5ded4310ca837554a371bb1da63e3019cd6d39'); // 替换成你的Appwrite API密钥
+        .setKey('43ae2735c77e33e0f0b04c60a35e84511839b5ec22dec5f2a119d98ca6402e7163f0e9140081d77c78cb31bf79af97f54f12eef91e290d70f7e98181f77455b8eb415b2f9747b129865bd5a8fa38a1b62d54788f68e21c1e496006ef885fb035a1d15836a80956b9813f58358f5ded4310ca837554a371bb1da63e3019cd6d39'); // 替换成你的Appwrite API密钥
 
     // 初始化核心服务（直接用全局Appwrite对象，无导出问题）
     const account = new window.Appwrite.Account(client);
@@ -40,3 +40,4 @@ window.addEventListener('load', async () => {
         collId: 'users'
     };
 });
+
